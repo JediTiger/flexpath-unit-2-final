217c930b019c4f313a49f2111daaaf117f49bbbc
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-27 13:34:30 -0500

    Starting ProductDao.java

---
+++ java-springboot-final/src/main/java/org/example/daos/ProductDao.java
@@ -0,0 +1,24 @@
+package org.example.daos;
+
+import org.springframework.stereotype.Component;
+
+/*
+  From the README.md, my task is to create DAO and controller classes for 3 of the 5 SQL tables provided:
+  Product, Order & OrderItem. So this should be 6 files. To start off small,
+  I'm starting with Product since that's the basis for everything else. You have to have products to order.
+
+
+
+
+
+
+
+
+
+
+ */
+// Added requirement Component
+@Component
+public class ProductDao {
+
+}
