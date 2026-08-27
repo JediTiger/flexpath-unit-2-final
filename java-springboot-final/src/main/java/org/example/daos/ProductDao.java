b51e93f2c6209a5384fc77f53c183608f4d2c926
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-27 17:00:27 -0500

    ProductDao.java shows no errors. ProductController next

--- java-springboot-final/src/main/java/org/example/daos/ProductDao.java
+++ java-springboot-final/src/main/java/org/example/daos/ProductDao.java
@@ -19,13 +19,15 @@ import java.sql.SQLException;
   You have to have products to order.
 */
 
+// Fields are id, product and price
+
 // Added requirement Component
 @Component
 public class ProductDao {
 
+   // Required declaration and constructor for JDBC
    private final JdbcTemplate jdbcTemplate;
 
-   // Constructor for ProductDao.java
    public ProductDao(DataSource dataSource) {
       this.jdbcTemplate = new JdbcTemplate(dataSource);
    }
@@ -37,9 +39,9 @@ public class ProductDao {
    // getAll - Retrieves all items from the table
    public List<Product> getAll() {
       String sql = "SELECT * FROM web_shop.products";
-      // Need something to map SQL items to the Product model
       return jdbcTemplate.query(sql, this::connectDBToProduct);
    }
+
    // getById - Retrieves an item by its id
    public Product getById(int id) {
       try {
@@ -50,11 +52,34 @@ public class ProductDao {
       }
    }
       // create product - Creates a new item in the table
+      public Product create(Product product) {
+         String sql = "INSERT INTO products (name, price) VALUES (?, ?);";
+         try {
+            jdbcTemplate.update(sql, product.getName(), product.getPrice());
+
+            String findSql = "SELECT * FROM products WHERE name = ? ORDER BY id DESC LIMIT 1;";
+            return jdbcTemplate.queryForObject(findSql, this::connectDBToProduct, product.getName());
+         } catch (Exception e) {
+            throw new DaoException("Failed to create product.");
+         }
+      }
 
       // update product - Updates an existing item in the table
+         public Product update(Product product) {
+            String sql = "UPDATE products SET name = ?, price = ? WHERE id = ?;";
+            int rowsAffected = jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getId());
+            if (rowsAffected == 0) {
+               throw new DaoException("Zero rows affected, expected at least one.");
+            } else {
+               return getById(product.getId());
+            }
+         }
 
       // delete product - Deletes an item from the table
-
+         public int delete(int id) {
+            String sql = "DELETE FROM products WHERE id = ?;";
+            return jdbcTemplate.update(sql, id);
+         }
 
 
       private Product connectDBToProduct(ResultSet resultSet, int rowNumber) throws SQLException {
